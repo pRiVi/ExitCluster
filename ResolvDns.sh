2e68d2c067a007f7741700a8687e84e8ab4768b5
Markus Schraeder <git@priv.de>
2015-06-20 22:05:10 +0200

    try multiple times to patch known hosts

--- ResolvDns.sh
+++ ResolvDns.sh
@@ -6,44 +6,55 @@ if [[ "$1" == "" ]]; then
 else 
    export HOST=$1;
 fi
+
 if [[ "`uname -a|grep mips`" == "" ]]; then
    export NOOPENWRT=1
 else
    echo Determined OpenWRT;
 fi
+
 export CURGW=`route -n|sed -e 's/0.0.0.0 \{1,\}\([0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}\) \{1,\}0.0.0.0.*/\1/p' -e 'd'`
+
 if [[ "$3" == "" ]]; then
    /bin/true;
 else
    export GW=$3
 fi
+
 if [[ "$GW" == "" ]]; then
    export GW=`cat /tmp/cur.gw`
 fi
+
 if ! `test -f /tmp/firstrun`; then
    if ! `test -f /usr/sbin/openvpn`; then
       if [[ "$NOOPENWRT" == "" ]]; then
          /usr/sbin/ntpd -p vpn.service
-         cat /root/.ssh/known_hosts|grep -v vpn.service >/root/.ssh/known_hosts.new;
-         echo vpn.service ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBcI28PxOM38w+8La4uYW1/oYG+uYDUPnLU4zBFSrzRC0laW0tE7KVzbzF7/3rCbwfrDG/Kz5ivxKdyrI64oms3P8TUYBbWrtZnSVRCjn8fpFKTD8f3W/C9K39jhp2ubRMFuLbcyS9XhnH9uLKMEgUakRs9DgndibXmN0ee3tIO1Qr+4qdw+4X399uXRalmlfEEjT7qSIg9oIIzJoX5QrI74jJXv61rNrgH/rZmwGwAVLP2oqevexh9vh4jmAZlUT6eb/RwXKRYtK4odcli1She9P2zTdszeVfvvhnwN2HmNDHvSyr7zvFstws2RpjUPCC5Y6VVT/5QSIx3RCd16yZ >>/root/.ssh/known_hosts.new;
-         mv /root/.ssh/known_hosts.new /root/.ssh/known_hosts
       fi
    fi
    touch /tmp/firstrun;
 fi
+
 if [[ "$GW" == "" ]]; then
    export GW=$CURGW
    echo $GW >/tmp/cur.gw
 fi
+
 if [[ "$2" == "" ]]; then
    export DNS=8.8.8.8
 else
    export DNS=$2
 fi
+
 if [[ "$4" == "" ]]; then
    export FAKEHOSTNAME=vpn.service
 else
    export FAKEHOSTNAME=$4
+fi
+
+if [[ "`grep $FAKEHOSTNAME /root/.ssh/known_hosts`"  == "" ]]; then
+   cat /root/.ssh/known_hosts|grep -v $FAKEHOSTNAME >/root/.ssh/known_hosts.new;
+   echo $FAKEHOSTNAME ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBcI28PxOM38w+8La4uYW1/oYG+uYDUPnLU4zBFSrzRC0laW0tE7KVzbzF7/3rCbwfrDG/Kz5ivxKdyrI64oms3P8TUYBbWrtZnSVRCjn8fpFKTD8f3W/C9K39jhp2ubRMFuLbcyS9XhnH9uLKMEgUakRs9DgndibXmN0ee3tIO1Qr+4qdw+4X399uXRalmlfEEjT7qSIg9oIIzJoX5QrI74jJXv61rNrgH/rZmwGwAVLP2oqevexh9vh4jmAZlUT6eb/RwXKRYtK4odcli1She9P2zTdszeVfvvhnwN2HmNDHvSyr7zvFstws2RpjUPCC5Y6VVT/5QSIx3RCd16yZ >>/root/.ssh/known_hosts.new;
+   mv /root/.ssh/known_hosts.new /root/.ssh/known_hosts
 fi
 
 if [[ "$GW" == "" ]]; then
